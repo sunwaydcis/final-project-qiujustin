0aecca0730ca4fdbff0e7a665838dfe51e323c29
qiujustin <qiu.justinaurelio@gmail.com>
2024-12-24 00:08:16 +0800

    Layout design

---
+++ src/main/scala-3/address/model/Person.scala
@@ -0,0 +1,10 @@
+package address.model
+
+import scalafx.beans.property.{StringProperty, IntegerProperty,
+  ObjectProperty}
+import java.time.LocalDate
+class Person ( firstNameS : String, lastNameS : String ):
+  var firstName = new StringProperty(firstNameS)
+  var lastName = new StringProperty(lastNameS)
+  var weight = new StringProperty("some Street")
+  var height = new StringProperty("some Street")
